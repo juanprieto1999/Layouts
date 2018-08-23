85084ab7c694a91f9b5614f6960fb0e66180740b
Roxana Coronel <ricoronel@misena.edu.co>
2018-08-23 14:56:30 -0500

    Initial commit

---
+++ Layouts/Ejercicio1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Layouts
+{
+    public partial class Ejercicio1 : ContentPage
+    {
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+    }
+}
